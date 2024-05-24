919ff35ed62b6c0e7556cdb4ede7cafd341fd75c
RoReke <rooo28@hotmail.com>
2024-05-24 08:22:23 -0300

    facturar many

--- app/view/facturacion.R
+++ app/view/facturacion.R
@@ -32,30 +32,49 @@ server <- function(id, db_manager) {
             useShinyjs(),
             fluidRow(
               dateInput(session$ns("date"), value = today(), label = "Fecha: ", format = "dd/mm/yyyy"),
+              textInput(session$ns("cliente"), label = "Cliente: ")
             ), 
+            hr(style="margin-top: 5px; margin-bottom: 5px;"),
             fluidRow(
-              column(6, 
-                selectInput(session$ns("articulo"), label = "Articulo", choices = productos$articulo, selected = NULL)
+              column(3, 
+                selectInput(session$ns("articulo"), label = "Articulo", choices = c("", productos$articulo), selected = NULL)
               ),
-              column(3,
-                numericInput(session$ns("cantidades"), label = "Cantidad: ", value = NULL)
+              column(9,
+                disabled(selectInput(session$ns("descripcion"), label = "Descripcion", choices = c("", productos$descripcion))) %>% 
+                  shiny::tagAppendAttributes(style = 'width: 100%;')
               )
             ),
             fluidRow(
-              column(6,
+              column(4,
+                numericInput(session$ns("cantidades"), label = "Cantidad: ", value = NULL)
+              ),
+              column(4,
                 numericInput(session$ns("talle"), label = "Talle: ", value = NULL)
               ),
-              column(6,
+              column(4,
                 textInput(session$ns("color"), label = "Color: ")
               )
             ),
             fluidRow(
-              disabled(selectInput(session$ns("descripcion"), label = "Descripcion", choices = productos$descripcion)) %>% 
-                shiny::tagAppendAttributes(style = 'width: 100%;')
+              column(5,
+                disabled(currencyInput(session$ns("precio_u"), label = "Precio Unitario:", value = NULL, format = "dollar"))
+              ),
+              column(5,
+                disabled(currencyInput(session$ns("precio_total"), label = "Subtotal: ", value = NULL, format = "dollar"))
+              ),
+              column(2,actionButton(session$ns("agregar"), "Agregar", icon = icon("plus"), style = "position: relative; top: 36px;"))
             ),
             fluidRow(
-              disabled(currencyInput(session$ns("precio_u"), label = "Precio Unitario:", value = NULL, format = "dollar")),
-              disabled(currencyInput(session$ns("precio_total"), label = "Total: ", value = NULL, format = "dollar"))
+              reactable::reactableOutput(session$ns("table_add"),width = "100%") 
+            ),
+            hr(style="margin-top: 5px; margin-bottom: 5px;"),
+            fluidRow(
+              column(5,
+                disabled(numericInput(session$ns("total_q"), label = "Total Cantidades:", value = NULL,))
+              ),
+              column(5,
+                disabled(currencyInput(session$ns("total"), label = "Total: ", value = NULL, format = "dollar"))
+              )
             ),
             div("Regla de negocio: >=6 Precio Mayorista")
           ),
@@ -64,6 +83,56 @@ server <- function(id, db_manager) {
       )
     })
     
+    data_table_add <- reactiveVal(data.frame(
+      articulo = character(),
+      cantidad = numeric(),
+      talle = integer(),
+      color = character(),
+      precio_u = numeric(),
+      precio_total = numeric(),
+      stringsAsFactors = FALSE
+    ))
+    
+    observeEvent(input$agregar, {
+      req(input$precio_total != 0)
+      req(input$cantidades)
+      req(input$articulo)
+      data <- data.frame(
+        articulo = input$articulo,
+        cantidad = input$cantidades,
+        talle = input$talle,
+        color = input$color,
+        precio_u = input$precio_u,
+        precio_total = input$precio_total
+      )
+      data_table_add(data_table_add() %>% bind_rows(data))
+      if (sum(data_table_add()$cantidad) >= 6) {
+        new_data <- data_table_add() %>% 
+          select(-c(precio_u, precio_total)) %>% 
+          left_join(db_manager$get_productos() %>% select(articulo, p_mayorista), by = "articulo") %>% 
+          mutate(
+            precio_u = p_mayorista,
+            precio_total = p_mayorista * cantidad
+          )
+        data_table_add(new_data)
+      }
+      shinyjs::reset("articulo")
+      shinyjs::reset("cantidades")
+      shinyjs::reset("talle")
+      shinyjs::reset("color")
+      shinyWidgets::updateCurrencyInput(inputId = "precio_u", value = 0)
+      shinyWidgets::updateCurrencyInput(inputId = "precio_total", value = 0)
+      
+      shinyWidgets::updateAutonumericInput(inputId = "total_q", value = sum(data_table_add()$cantidad))
+      shinyWidgets::updateCurrencyInput(inputId = "total", value = sum(data_table_add()$precio_total))
+    }) 
+    
+    output$table_add <- reactable:::renderReactable({
+      req(data_table_add())
+      reactable::reactable(
+        data_table_add()
+      )
+    })
     observeEvent(input$articulo, {
       productos <- db_manager$get_productos()
       updateSelectInput(inputId = "descripcion", selected = productos %>% filter(articulo == input$articulo) %>% pull(descripcion))
@@ -87,19 +156,17 @@ server <- function(id, db_manager) {
     
     update_table <- reactiveVal(0)
     observeEvent(input$confirmar_facturar, {
-      req(input$precio_total != 0)
-      req(input$cantidades)
-      req(input$articulo)
-      
+      req(nrow(data_table_add() > 0))
       data <- data.frame(
         id = uuid::UUIDgenerate(),
         fecha = input$date,
-        articulo = input$articulo,
-        cantidad = input$cantidades,
-        talle = input$talle,
-        color = input$color,
-        precio_venta_unitario = input$precio_u,
-        precio_total = input$precio_total
+        cliente = input$cliente,
+        articulo = data_table_add()$articulo,
+        cantidad = data_table_add()$cantidad,
+        talle = data_table_add()$talle,
+        color = data_table_add()$color,
+        precio_venta_unitario = data_table_add()$precio_u,
+        precio_total = data_table_add()$precio_total
       )
       
       db_manager$append_to_table(
@@ -109,6 +176,16 @@ server <- function(id, db_manager) {
       
       removeModal()
       update_table(update_table() + 1)
+      
+      data_table_add(data.frame(
+        articulo = character(),
+        cantidad = numeric(),
+        talle = integer(),
+        color = character(),
+        precio_u = numeric(),
+        precio_total = numeric(),
+        stringsAsFactors = FALSE
+      ))
     })
     
     output$table <- renderReactable({
